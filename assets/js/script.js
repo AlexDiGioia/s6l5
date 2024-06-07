868af1e675a30ba5f079bec1da8b69ba9bfd1a4f
AlexDiGioia <digioia.alex2001@gmail.com>
2024-06-07 17:00:53 +0200

    carosello da finire

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll(".carousel .carousel-item");
+items.forEach((e) => {
+  const slide = 5;
+  let next = e.nextElementSibling;
+  for (let i = 0; i < slide; i++) {
+    if (!next) {
+      next = items[0];
+    }
+    let clonechild = next.cloneNode(true);
+    e.appendChild(clonechild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
